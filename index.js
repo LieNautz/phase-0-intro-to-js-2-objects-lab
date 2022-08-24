1dcec86d4d3bf8a85aaa48882ef15a6879d5b591
LieNautz <tuan_lai01@hotmail.com>
2022-08-24 22:24:26 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+let employee= {
+    name:'Sam',
+    streetAddress:'11 Broadway'
+}
+
+//Make this function nondestructive
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+const newObject= {...employee};
+
+newObject[key]=value;
+
+return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+ employee[key]=value;
+
+ return employee;
+ 
+}
+
+//This is nondestructive
+function deleteFromEmployeeByKey(employee,key) {
+let newObj={...employee,};
+
+delete newObj[key];
+
+return newObj;
+
+}
+
+//Make this function destructive
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+delete employee[key]
+return employee
+
+}
